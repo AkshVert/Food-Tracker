5bbde4ebbbcdb1c6958c6c0cc4cbf9dbc5492723
maniac <maniac@maniac.myguest.virtualbox.org>
2023-03-24 12:46:01 +0530

    Added Files

---
+++ database.py
@@ -0,0 +1,28 @@
+from flask import g
+import sqlite3
+
+def connect_db():
+    sql = sqlite3.connect('/home/maniac/workspace/flask-food-app/23/questions.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+    
+def init_db():
+    db = connect_db()
+
+    db[1].execute(open('schema.sql', 'r').read())
+    db[1].close()
+
+    db[0].close()
+
+def init_admin():
+    db = connect_db()
+
+    db[1].execute('update users set admin = True where name = %s', ('admin', ))
+
+    db[1].close()
+    db[0].close()
